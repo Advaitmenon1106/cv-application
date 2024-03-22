c45c98b52cf1dc61c99a23a36f8e07767d20771c
Advaitmenon1106 <advaitmenon1106@gmail.com>
2024-03-23 00:48:18 +0530

    Remove the commented portion of the code since it is now redundant as a better way of coding the states is implemented.

--- src/Input-Side/logic/AboutUser.jsx
+++ src/Input-Side/logic/AboutUser.jsx
@@ -1,35 +1,35 @@
 import '../styling/AboutUser.css'
 
-export function ApplicantDetails({onChangeFn = null} /*{ onChangeEmail = null, onChangeGH = null, onChangeLinkedIn = null, onChangePhoneNo = null, onChangeName = null }*/) {
+export function ApplicantDetails({ onChangeFn = null }) {
     return (
         <>
             <div id='contact-details'>
 
                 <div>
                     <label>Email ID: </label>
-                    <input name='email' onChange={onChangeFn} /*onChange={onChangeEmail}*/ />
+                    <input name='email' type='email' onChange={onChangeFn} />
+                    {/* <input type='color' /> */}
                 </div>
 
                 <div>
                     <label>GitHub Account: </label>
-                    <input name='gh' onChange={onChangeFn} /*onChange={onChangeGH}*/ />
+                    <input name='gh' onChange={onChangeFn} />
                 </div>
 
                 <div>
                     <label>LinkedIn: </label>
-                    <input name='linkedin' onChange={onChangeFn} /*onChange={onChangeLinkedIn}*/ />
+                    <input name='linkedin' onChange={onChangeFn} />
                 </div>
 
                 <div>
                     <label>Phone Number: </label>
-                    <input name='phoneNo' onChange={onChangeFn} /*onChange={onChangePhoneNo}*/ />
+                    <input name='phoneNo' type='number' id='phone-number' onChange={onChangeFn} />
                 </div>
 
                 <label>Full Name: </label>
-                <input name='fullname' onChange={onChangeFn} /*onChange={onChangeName}*/ />
+                <input name='fullname' onChange={onChangeFn} />
 
             </div>
-            {/* <hr className='line-break'></hr> */}
         </>
     )
 }
