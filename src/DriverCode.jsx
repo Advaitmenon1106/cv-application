7c7aeb68c2940318383bb661bd105986c20c2936
Advaitmenon1106 <advaitmenon1106@gmail.com>
2024-03-23 00:56:26 +0530

    Add on all the new components to the Consortium component.

--- src/DriverCode.jsx
+++ src/DriverCode.jsx
@@ -1,6 +1,9 @@
 import { ApplicantDetails } from "./Input-Side/logic/AboutUser";
 import './Driver.css'
 import { useState } from "react";
+import { ApplicantDetailOutputs } from "./Output-Side/logic/ApplicantDetails";
+import { ApplicantSkills } from "./Input-Side/logic/ApplicantSkills";
+import { SkillField } from "./Output-Side/logic/ApplicantSkillField";
 
 export function Consortium() {
     const [value, setValue] = useState({
@@ -11,12 +14,18 @@ export function Consortium() {
         'fullname': ''
     });
 
+    const [skill, setSkillValue] = useState('');
+
 
     const onModifyingVal = (event) => {
         setValue({
             ...value,
             [event.target.name]: event.target.value
-        })
+        });
+    }
+
+    const onModifyingSkillField = (event) => {
+        setSkillValue(event.target.value);
     }
 
     return (
@@ -24,12 +33,15 @@ export function Consortium() {
             <table>
                 <tbody>
                     <tr>
-                        <td id="input-side-td"> <ApplicantDetails onChangeFn={onModifyingVal} /> </td>
-                        <td>
-                            <div>{value['email']}</div>
-                            <div>{value['gh']}</div>
-                            <div>{value['linkedin']}</div>
-                            <div>{value['phoneNo']}</div>
+                        <td id="input-side-td">
+                            <ApplicantDetails onChangeFn={onModifyingVal} />
+                            <ApplicantSkills onChangeFn={onModifyingSkillField} />
+                        </td>
+                        <td id="output-side-td">
+                            <div className="a4-cv">
+                                <ApplicantDetailOutputs value={value} />
+                                <SkillField skills={skill}/>
+                            </div>
                         </td>
                     </tr>
                 </tbody>
