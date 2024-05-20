3c036db28a9a7936b0aad525ae4f265b86426c97
Advaitmenon1106 <advaitmenon1106@gmail.com>
2024-05-20 21:13:50 +0530

    Include a second div while traversing the qualification object's keys. This helps access the dates as well.

--- src/Output-Side/logic/DisplayApplicantEducation.jsx
+++ src/Output-Side/logic/DisplayApplicantEducation.jsx
@@ -1,11 +1,17 @@
 import '../styling/ApplicantEducationDisplay.css'
 
 
-export function ApplicantQualifications({ qualifications }) {
+export function ApplicantQualifications({ qualifications, dates }) {
     return (
         <div>
             <div className="headers">Education</div>
-            <div>{Object.keys(qualifications).map((key) => { return <div key={key} className='education'>{qualifications[key]}</div> })}</div>
+            <div>{Object.keys(qualifications).map((key) => {
+                return <div key={key} className='education'>
+                    <div className='qual'>{qualifications[key]}</div>
+                    <div className='date'>{dates[key]}</div>
+                </div>
+            })}
+            </div>
         </div>
     )
 }
